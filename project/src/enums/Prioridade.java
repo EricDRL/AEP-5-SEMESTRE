af2cbf37bf1411a3b0cdba4c5cc89cb6658c4c62
BrunoFujisaki <brunofujisaki2004@gmail.com>
2026-04-08 22:47:10 -0300

    feat: enums 'prioridade' e 'statusSolicitacao' customizado com descrição associada

--- project/src/enums/Prioridade.java
+++ project/src/enums/Prioridade.java
@@ -1,8 +1,18 @@
 package enums;
 
 public enum Prioridade {
-    BAIXA,
-    MEDIA,
-    ALTA,
-    URGENTE
+    BAIXA("Baixa"),
+    MEDIA("Média"),
+    ALTA("Alta"),
+    URGENTE("Urgente");
+
+    private final String descricao;
+
+    Prioridade(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 }
--- project/src/enums/StatusSolicitacao.java
+++ project/src/enums/StatusSolicitacao.java
@@ -1,9 +1,19 @@
 package enums;
 
 public enum StatusSolicitacao {
-    ABERTO,
-    TRIAGEM,
-    EM_EXECUCAO,
-    RESOLVIDO,
-    ENCERRADO
+    ABERTO("Aberto"),
+    TRIAGEM("Triagem"),
+    EM_EXECUCAO("Em execução"),
+    RESOLVIDO("Resolvido"),
+    ENCERRADO("Encerrado");
+
+    private final String descricao;
+
+    StatusSolicitacao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 }
